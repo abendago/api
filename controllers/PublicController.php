776d0d42dd14849c0eef8f4973a4d7029e5f3f9a
Nathan Leggatt <nathan@vanarts.com>
2020-11-02 12:53:18 -0800

    initial base API

---
+++ controllers/PublicController.php
@@ -0,0 +1,12 @@
+<?php
+Class PublicController extends Controller{
+
+	var $content = "";
+
+	public function main(){
+
+		// $this->loadView("views/login.php");
+		// $this->loadLastView("views/main.php");
+	}
+
+}
